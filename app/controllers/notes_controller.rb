f7c7a676d93e420c8e7305eac510785224e8fe46
Evan <evanfujita@gmail.com>
2022-05-19 18:36:46 -0500

    wip

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,26 @@
 class NotesController < ApplicationController
+
+    def new
+        @note = Note.new
+    end
+
+    def index
+        @notes = Note.all
+    end
+
+    def create
+        note = Note.create(note_params)
+        pp "\n\n\n\n #{note} \n\n\n"
+        render index
+    end
+
+    def show
+        @note = Note.find(params[:id])
+    end
+
+    private
+
+    def note_params
+        params.require(:note).permit(:date, :title, :body, :lesson)
+    end
 end
